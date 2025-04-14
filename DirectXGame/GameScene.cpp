eed598458bfae91f30977a64431265d2bb6caafb
Kimjunsoo1032 <k024g1032@g.neec.ac.jp>
2025-04-14 11:50:03 +0900

    START

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,14 @@
+#include "GameScene.h"
+
+using namespace KamataEngine;
+
+void GameScene::Initialize() {
+	//メンバ変数への代入処理(省略)
+	//ここにインゲームの初期化処理を書く
+}
+void GameScene::Update() {
+	//ここにインゲームの更新処理を書く
+}
+void GameScene::Draw() {
+	
+}
